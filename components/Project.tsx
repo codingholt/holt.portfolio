dde9f44c9fdcfc47e0a8b9aba6ed25090a3c156a
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-07-13 15:11:11 +0200

    nice card

--- components/Project.tsx
+++ components/Project.tsx
@@ -6,7 +6,7 @@ function Project({ProjectTitle, ProjectImg, ProjectLink, ProjectDescription, Pro
   return (
 
     <Parallax key={ProjectId}>
-    <div className="wrapper bg-gray-400 antialiased text-gray-900">
+    {/* <div className="wrapper bg-gray-400 antialiased text-gray-900">
     <div  onClick={() => window.open(ProjectLink, '_blank')}>
         
     </div>
@@ -32,6 +32,32 @@ function Project({ProjectTitle, ProjectImg, ProjectLink, ProjectDescription, Pro
 
 
     </div>
+ */}
+
+
+<div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 hover:opacity-80 transform transition duration-500 hover:scale-105">
+    <a href={ProjectLink}>
+        <Image className="rounded-t-lg max-w-xs" 
+                src={ProjectImg} 
+                objectFit='cover'
+                layout='intrinsic'
+                width={1000}
+                height={500}
+                alt={'Cover Image' + ProjectTitle}
+              />    
+    </a>
+    <div className="p-5">
+        <a href={ProjectLink}>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{ProjectTitle}</h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{ProjectDescription}</p>
+        {/* <a href={ProjectLink} className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          P
+            <svg class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </a> */}
+    </div>
+</div>
+
     </Parallax>
   )
 }
