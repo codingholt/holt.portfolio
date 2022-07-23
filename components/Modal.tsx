28d5a551d8c4116b30877f1dc28eca5af1761dd6
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-07-23 15:30:54 +0200

    added modal

---
+++ components/Modal.tsx
@@ -0,0 +1,67 @@
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+
+export default function Modal({ modalTitle, modalDescription, modalOpen, closeModal}:{ modalTitle: string, modalDescription: string, modalOpen: any, closeModal: any}) {
+
+
+  return (
+    <>
+
+      <Transition appear show={modalOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    {modalTitle}
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      {modalDescription}
+                    </p>
+                  </div>
+
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="inline-block bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 mt-4 px-4 border border-gray-400 rounded shadow hover:cursor-pointer"
+                      onClick={() => window.open('mailto:codingholt@gmx.com', '_blank')}
+                    >
+                      Open Email
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
+}
+
+
